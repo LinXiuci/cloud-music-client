a9d8f0c0cee59d4a0b1c0fff73404d582176bcb9
xiewenqi_1030@163.com <xiewenqi_1030@163.com>
2022-09-05 23:51:57 +0800

    README.md

--- src/api/request.ts
+++ src/api/request.ts
@@ -34,7 +34,6 @@ axios.interceptors.response.use(
           });
           break;
         case 403:
-          // console.log('管理员权限已修改请重新登录')
           // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
           setTimeout(() => {
             router.replace({
