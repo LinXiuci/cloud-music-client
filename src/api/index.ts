b0249cd8cbb97406471564e1e9b4c8ec76e169b8
xiewenqi_1030@163.com <xiewenqi_1030@163.com>
2022-09-06 10:04:36 +0800

    修改一些api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { getBaseURL, get, post } from "./request";
 const HttpManager = {
   // 获取图片信息
   attachImageUrl: (url) => url ? `${getBaseURL()}/${url}` : "https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png",
-  // =======================> 用户 API
+  /* ----------------------------  用户 API  ----------------------------*/
   // 登录
   signIn: (params) => post(`user/login/status`, params),
   // 注册
@@ -18,7 +18,7 @@ const HttpManager = {
   // 更新用户头像
   uploadUrl: (userId) => `${getBaseURL()}/user/avatar/update?id=${userId}`,
 
-  // =======================> 歌单 API
+  /* ----------------------------  歌单 API ----------------------------*/
   // 获取全部歌单
   getSongList: () => get("songList"),
   // 获取歌单类型
@@ -28,13 +28,13 @@ const HttpManager = {
   // 返回歌单里指定歌单ID的歌曲
   getListSongOfSongId: (songListId) => get(`listSong/detail?songListId=${songListId}`),
 
-  // =======================> 歌手 API
+  /* ----------------------------  歌手 API ----------------------------*/
   // 返回所有歌手
   getAllSinger: () => get("singer"),
   // 通过性别对歌手分类
   getSingerOfSex: (sex) => get(`singer/sex/detail?sex=${sex}`),
 
-  // =======================> 收藏 API
+  /* ----------------------------  收藏 API ----------------------------*/
   // 返回的指定用户ID的收藏列表
   getCollectionOfUser: (userId) => get(`collection/detail?userId=${userId}`),
   // 添加收藏的歌曲 type: 0 代表歌曲， 1 代表歌单
@@ -44,7 +44,7 @@ const HttpManager = {
 
   isCollection: (params) => post(`collection/status`, params),
 
-  // =======================> 评分 API
+  /* ---------------------------- 评分 API ----------------------------*/
   // 提交评分
   setRank: (params) => post(`rankList/add`, params),
   // 获取指定歌单的评分
@@ -52,7 +52,9 @@ const HttpManager = {
   // 获取指定用户的歌单评分
   getUserRank: (consumerId, songListId) => get(`/rankList/user?consumerId=${consumerId}&songListId=${songListId}`),
 
-  // =======================> 评论 API
+
+
+  /* ---------------------------- 评论 API  ----------------------------*/
   // 添加评论
   setComment: (params) => post(`comment/add`, params),
   // 删除评论
@@ -70,7 +72,7 @@ const HttpManager = {
     return get(url);
   },
 
-  // =======================> 歌曲 API
+  /* ---------------------------- 歌曲 API  ----------------------------*/
   // 返回指定歌曲ID的歌曲
   getSongOfId: (id) => get(`song/detail?id=${id}`),
   // 返回指定歌手ID的歌曲
